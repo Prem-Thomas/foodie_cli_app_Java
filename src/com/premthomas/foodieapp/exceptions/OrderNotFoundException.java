8fedd3003b67f60fea78fc9a2d1d54e76721a736
Prem-Thomas <erapogupremaraju158200@gmail.com>
2024-10-14 19:28:05 +0530

    "feat:Add OrderNotFoundException for Order Class"

---
+++ src/com/premthomas/foodieapp/exceptions/OrderNotFoundException.java
@@ -0,0 +1,7 @@
+package com.premthomas.foodieapp.exceptions;
+
+public class OrderNotFoundException extends Exception{
+    public OrderNotFoundException(String message){
+        super(message);
+    }
+}
