d018205658591febb5584a17143e535909fb9a03
Prem-Thomas <erapogupremaraju158200@gmail.com>
2024-10-14 19:30:57 +0530

    "feat:Add RestaurantExistsException for restaurant Class"

---
+++ src/com/premthomas/foodieapp/exceptions/RestaurantExistsException.java
@@ -0,0 +1,7 @@
+package com.premthomas.foodieapp.exceptions;
+
+public class RestaurantExistsException extends Exception{
+    public  RestaurantExistsException(String message){
+        super(message);
+    }
+}
