0c1798b479cb1f98aaf48d4e0b40445f070dbd90
Prem-Thomas <erapogupremaraju158200@gmail.com>
2024-10-14 20:15:40 +0530

    "feat:Add Repository Class"

---
+++ src/com/premthomas/foodieapp/repository/CustomerRepository.java
@@ -0,0 +1,52 @@
+package com.premthomas.foodieapp.repository;
+
+import com.premthomas.foodieapp.model.Customer;
+
+import javax.swing.text.html.Option;
+import java.util.List;
+import java.util.Optional;
+
+public class CustomerRepository {
+    List<Customer> customerList;
+
+    public Customer saveCustomerList(Customer customer){
+         this.customerList.add(customer);
+         return customer;
+    }
+// Find the Customer Details by the Id
+    public Optional<Customer> findCustomerById(String id){
+        return this.customerList.stream().filter(customer -> customer.getId().equals(id)).findFirst();
+    }
+// Find the Customer Details by the Email
+    public Optional<Customer> findCustomerByEmail(String email){
+        return this.customerList.stream().filter(customer -> customer.getEmail().equals(email)).findFirst();
+    }
+  // For Update the Customer Details
+    public Customer updateCustomer(Customer customerToBeUpdated){
+        Optional<Customer> updateCustomer = this.customerList.stream().filter(customer -> customer.getId().equals(customerToBeUpdated))
+                .findFirst()
+                .map(customer -> {
+                    customer.setName(customerToBeUpdated.getName())
+                            .setEmail(customerToBeUpdated.getEmail())
+                            .setPassword(customerToBeUpdated.getPassword());
+                    return customer;
+                });
+        return updateCustomer.orElse(null);
+    }
+ // Get the values From the Customer List
+
+    public List<Customer> getCustomerList(){
+        return this.customerList;
+    }
+
+// To Delete the Customer When We don't need
+
+    public void deleteCustomer(Customer customer){
+        this.customerList.remove(customer);
+    }
+
+    // Find the Details of Customer By Email and Password
+    public Optional<Customer> findByEmailAndPassword(String email,String password){
+        return this.customerList.stream().filter(customer -> customer.getEmail().equalsIgnoreCase(email) && customer.getPassword().equals(password)).findFirst();
+    }
+}
