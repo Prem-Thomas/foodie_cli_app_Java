82ea4b4f3bc1404373d6e96fb0226ecda1235238
Prem-Thomas <erapogupremaraju158200@gmail.com>
2024-10-14 19:24:16 +0530

    "feat:Add NotFoundException for Dish Class"

---
+++ src/com/premthomas/foodieapp/exceptions/DishExistsException.java
@@ -0,0 +1,8 @@
+package com.premthomas.foodieapp.exceptions;
+
+public class DishExistsException extends Exception{
+   public  DishExistsException(String message){
+       super(message);
+   }
+}
+
