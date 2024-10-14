b67b5cfc1ca656667d71909786daf578de7e21fc
Prem-Thomas <erapogupremaraju158200@gmail.com>
2024-10-14 19:22:17 +0530

    "feat:Add Exception for Customer CRUD operation"

---
+++ src/com/premthomas/foodieapp/exceptions/CustomerNotFoundException.java
@@ -0,0 +1,7 @@
+package com.premthomas.foodieapp.exceptions;
+
+public class CustomerNotFoundException extends Exception{
+    public CustomerNotFoundException(String message){
+        super(message);
+    }
+}
