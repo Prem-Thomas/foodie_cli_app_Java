f03af61b16e321900617bb22c99757412dada078
Prem-Thomas <erapogupremaraju158200@gmail.com>
2024-11-13 21:20:51 +0530

    "feat:Main Method"

--- src/com/premthomas/foodieapp/Main.java
+++ src/com/premthomas/foodieapp/Main.java
@@ -1,10 +1,12 @@
 package com.premthomas.foodieapp;
 
+import com.premthomas.foodieapp.ui.Menu;
 import com.premthomas.foodieapp.util.CsvReader;
 
 public class Main {
     public static void main(String[] args) {
-        CsvReader csvReader = new CsvReader();
-        System.out.println(csvReader.readDishesFromCsv());
+       Menu menu = new Menu();
+       menu.displayMenu();
+
     }
 }
